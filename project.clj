2e69f5161d46ff1393290da4b848fa15547524e1
Simon Brooke <simon@journeyman.cc>
2020-02-01 13:25:47 +0000

    lein-release plugin: preparing 0.1.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject geocsv "0.1.2-SNAPSHOT"
+(defproject geocsv "0.1.2"
 
   :description "A wee tool to show comma-separated value data on a map."
   :url "https://github.com/simon-brooke/geocsv"
